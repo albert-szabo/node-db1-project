9d54eeaa5caf5977842477e8e3f6436831e65f02
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-06-13 18:39:17 -0700

    Completed two additional middleware functions

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,38 @@
 const Accounts = require('./accounts-model');
 
+const database = require('../../data/db-config');
+
 exports.checkAccountPayload = (request, response, next) => {
-  // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
+
+  const { name, budget} = request.body;
+  if (name === undefined || budget === undefined) {
+    next({ status: 400, message: 'name and budget are required' });
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    next({ status: 400, message: 'name of account must be between 3 and 100' });
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    next({ status: 400, message: 'budget of account must be a number' });
+  } else if (budget < 0 || budget > 1000000) {
+    next({ status: 400, message: 'budget of account is too large or too small' });
+  } else {
+    request.name = name;
+    request.budget = budget;
+    next();
+  }
 };
 
-exports.checkAccountNameUnique = (request, response, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (request, response, next) => {
+  try {
+    const existingAccount = await database('accounts').where('name', request.body.name.trim()).first();
+    if (existingAccount) {
+      next({ status: 400, message: 'that name is taken' });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.checkAccountId = async (request, response, next) => {
